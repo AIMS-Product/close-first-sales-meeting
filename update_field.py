6891d402169ace660d5d90ef7632fb8d3bdeed12
Dom Ellis <dom@aimanagingservices.com>
2026-09-15 16:09:43 -0700

    Update Raiya catalyst meeting title

--- update_field.py
+++ update_field.py
@@ -146,7 +146,7 @@ SCRAPER_TITLE_MAP = [
     (re.compile(r"vendingpren[eu]+rs?\s+compass\s*-?\s*next\s+steps", re.IGNORECASE), "Josh Stoffel"),  # Vendingpreneurs Compass - Next Steps
     (re.compile(r"vendingpren[eu]+rs?\s+horizon\s*-?\s*next\s+steps", re.IGNORECASE), "Beatrice Braescu Cojocaru"),  # Vendingpreneurs Horizon - Next Steps
     (re.compile(r"vendingpren[eu]+rs?\s+elevate\s*-?\s*next\s+steps", re.IGNORECASE), "Catalina"),  # Vendingpreneurs Elevate - Next Steps
-    (re.compile(r"vendingpren[eu]+rs?\s+catalyst\s*-?\s*next\s+steps", re.IGNORECASE), "Raiya"),  # Vendingpreneurs Catalyst - Next Steps
+    (re.compile(r"ppa\s+catalyst\s*-?\s*next\s+steps", re.IGNORECASE), "Raiya"),  # PPA Catalyst - Next Steps
     (re.compile(r"vendingpren[eu]+rs?\s+clarity\s*-?\s*next\s+steps", re.IGNORECASE), "Luna"),  # Vendingpreneurs Clarity - Next Steps
 ]
 
